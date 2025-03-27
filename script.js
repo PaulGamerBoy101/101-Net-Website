e586f67cd697f1688791c54fb32c57365ebc5a6a
PaulGamerBoy101 <166418808+PaulGamerBoy101@users.noreply.github.com>
2025-03-27 17:23:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Set the download button link directly
-    document.getElementById("download-btn").href = "https://github.com/PaulGamerBoy101/101-Net-Website/archive/refs/heads/main.zip";
+    document.getElementById("download-btn").href = "https://github.com/PaulGamerBoy101/101-Net/archive/refs/heads/main.zip";
 });
